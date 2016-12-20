e5e82425e3ea6868e353a01f33402e302d078cee
Felix Guo <felix@Zhongqis-iMac.lan>
2016-12-20 00:20:25 -0800

    Remove npm libraries

--- index.android.js
+++ index.android.js
@@ -4,13 +4,21 @@
  * @flow
  */
 
+
+ 'use strict';
+
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  ScrollView,
+  Image,
+  Picker
 } from 'react-native';
+
+//var SearchBar = require('react-native-search-bar');
 
 export default class android_client extends Component {
   render() {
@@ -19,7 +27,20 @@ export default class android_client extends Component {
         <Text style={styles.welcome}>
             Hello, World!
         </Text>
-      </View>
+         <ScrollView>
+          <Text style={{fontSize:32}}>Scroll me plz</Text>
+          <Image source={require('./img/zoro.jpeg')} />
+          <Image source={require('./img/zoro-naked.jpeg')} />
+          <Image source={require('./img/zoro.jpeg')} />
+        </ScrollView>
+        <Picker
+          selectedValue={this.state.language}
+          onValueChange={(lang) => this.setState({language: lang})}>
+          <Picker.Item label="Java" value="java" />
+          <Picker.Item label="JavaScript" value="js" />
+        </Picker>
+        </View>
+
     );
   }
 }
